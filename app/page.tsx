7be1d54e7934febc4bb552736aae1c9ed3b76d36
Heitor Stael <heitorstael@live.com>
2024-09-04 13:42:03 -0300

    Next solid base + MUI

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <p className="m-0 max-w-[30ch] text-sm opacity-50">
+        Explore starter templates for Next.js.
+      </p>
+    </main>
+  );
+}
